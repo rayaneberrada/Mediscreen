94f2de0be6271264b1dfa74259bca4f192a48000
rayaneberrada <rayaneberrada44500@gmail.com>
2021-03-21 20:32:24 +0100

    project created and pom setted up with a first microservice

---
+++ patients-infos/src/main/java/fr/openclassrooms/rayane/patientsinfos/PatientsInfosApplication.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.rayane.patientsinfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsInfosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsInfosApplication.class, args);
+	}
+
+}
---
+++ patients-infos/src/test/java/fr/openclassrooms/rayane/patientsinfos/PatientsInfosApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.rayane.patientsinfos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsInfosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
